58c2c519776a7cbff545c606f0082b1056da8422
David Watkins <dwatkins@insuremonkey.com>
2015-04-16 16:38:20 -0700

    Update model to allow for no user/groups attached to actions

--- djangoacl/models.py
+++ djangoacl/models.py
@@ -5,5 +5,5 @@ from django.utils.translation import ugettext_lazy as _
 
 class Action(models.Model):
     name = models.CharField(verbose_name=_('Action Name'), max_length=20, unique=True)
-    users = models.ManyToManyField('auth.User', verbose_name=_('Users'), related_name='actions')
-    groups = models.ManyToManyField('auth.Group', verbose_name=_('Groups'), related_name='actions')
+    users = models.ManyToManyField('auth.User', verbose_name=_('Users'), related_name='actions', null=True, blank=True)
+    groups = models.ManyToManyField('auth.Group', verbose_name=_('Groups'), related_name='actions', null=True, blank=True)
